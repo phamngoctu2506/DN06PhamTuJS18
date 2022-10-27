66d8e1efea084e1122664633d3f1f1e1f06976a8
Phamtu <phamngoctu2506@gmail.com>
2022-10-27 18:36:59 +0700

    up

--- js/main.js
+++ js/main.js
@@ -1,5 +1,49 @@
 /**
- * bài tập 1:
- * 
- * 
+ * Thêm các Số vào mảng
  */
+
+var numArray = [];
+
+function themSo() {
+    var num1 = Number(document.querySelector('#num1').value);
+    numArray.push(num1);
+    document.querySelector('#ketQua').innerHTML = numArray;
+}
+
+//! bài 1: tính tổng số dương
+
+function TongSoDuong() {
+    var tongCacSo = 0;
+    for (var i = 0; i < numArray.length; i++) {
+        if (numArray[i] >= 0) {
+            tongCacSo += numArray[i];
+        }
+    }
+    document.querySelector('#ketQua1').innerHTML = tongCacSo;
+}
+
+//! bài 2: đếm có bao nhiêu số dương trong mảng
+
+function DemSoDuong() {
+    var tongSoDuong = 0;
+    for (var i = 0; i < numArray.length; i++) {
+        if (numArray[i] >= 0) {
+            tongSoDuong++;
+        }
+    }
+    document.querySelector('#ketQua2').innerHTML = tongSoDuong;
+}
+
+//! bài 3: tìm số nhỏ nhất
+
+function TimSoNhoNhat() {
+    var min = numArray[0,1,2,3];
+    var viTri = 0;
+    for (var i = 0; i < numArray.length; i++) {
+        if (min > numArray[i]) {
+            min = numArray[i];
+            viTri = i;
+        }
+    }
+    document.querySelector('#ketQua3').innerHTML = min;
+}
